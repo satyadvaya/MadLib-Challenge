53fc2566629644790e4059b52692875e6c5e2c28
satyadvaya <satyadvaya@gmail.com>
2021-06-25 16:07:55 -0700

    begun javascript

---
+++ app.js
@@ -0,0 +1,31 @@
+const nonsenseLocation = document.getElementById("non-sense-loc-input");
+console.log(nonsenseLocation);
+const veggiesInput = document.getElementById("veggies-input");
+console.log(veggiesInput);
+const woodsInput = document.getElementById("woods-input");
+console.log(woodsInput);
+const firstNameInput = document.getElementById("first-name-input");
+console.log(firstNameInput);
+const middleNameInput = document.getElementById("middle-name-input");
+console.log(middleNameInput);
+const lastNameInput = document.getElementById("last-name-input");
+console.log(lastNameInput);
+const furnitureInput = document.getElementById("furniture-input");
+console.log(furnitureInput);
+const fireInput = document.getElementById("fire");
+const jugInput = document.getElementById("jug-input");
+console.log(jugInput);
+
+const button = document.getElementById("button");
+console.log(button);
+
+// spans
+const firstLocation = document.getElementById("non-sense-loc");
+const vegetables = document.getElementsByClassName("vegetables");
+const woods = document.getElementsByClassName("sticks");
+const firstName = document.getElementsByClassName("first-name");
+const middleName = document.getElementsByClassName("middle-name");
+const lastName = document.getElementsByClassName("last-name");
+const chairs = document.getElementsByClassName("furniture");
+const candle = document.getElementsByClassName("fire");
+const drinkingVessel = document.getElementsByClassName("drinking-vessel");
