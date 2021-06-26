dc4adf444b2d44b7934c8f0f310300db31f41b6e
Cristian Montes <cmontesmujica@gmail.com>
2021-06-26 14:14:28 -0700

    functions added to all inputs

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
-const nonsenseLocation = document.getElementById("non-sense-loc-input");
-const veggiesInput = document.getElementById("veggies-input");
-const woodsInput = document.getElementById("woods-input");
-const firstNameInput = document.getElementById("first-name-input");
-const middleNameInput = document.getElementById("middle-name-input");
-const lastNameInput = document.getElementById("last-name-input");
-const furnitureInput = document.getElementById("furniture-input");
-const fireInput = document.getElementById("fire");
-const jugInput = document.getElementById("jug-input");
-const button = document.getElementById("button");
+// INPUTS
+const nonsenseLocInput= document.getElementById("non-sense-loc-input"); //
+const veggiesInput = document.getElementById("veggies-input");  ////
+const woodsInput = document.getElementById("woods-input");  ////
+const firstNameInput = document.getElementById("first-name-input"); ////
+const middleNameInput = document.getElementById("middle-name-input"); ////
+const lastNameInput = document.getElementById("last-name-input"); ////
+const furnitureInput = document.getElementById("furniture-input"); //
+const fireInput = document.getElementById("candle-input"); //
+const jugInput = document.getElementById("jug-input"); //
+const button = document.getElementById("button"); //
 
-// spans
-const firstLocation = document.getElementById("non-sense-loc");
-const pumpkin = document.getElementById("vegetables");
-const woods = document.getElementById("sticks");
-const firstName = document.getElementById("first-name");
-const middleName = document.getElementById("middle-name");
-const lastName = document.getElementById("last-name");
-const chairs = document.getElementById("furniture");
-const candle = document.getElementById("fire");
-const drinkingVessel = document.getElementById("drinking-vessel");
+// SPANS
+const firstLocation = document.getElementById("non-sense-loc"); //
+const vegetables = document.getElementsByClassName("vegetables"); ////
+const woods = document.getElementsByClassName("sticks");  ////
+const firstName = document.getElementsByClassName("first-name"); ////
+const middleName = document.getElementsByClassName("middle-name"); ////
+const lastName = document.getElementsByClassName("last-name");
+const chairs = document.getElementById("furniture");  //
+const candle = document.getElementById("fire"); //
+const drinkingVessel = document.getElementById("drinking-vessel");//
 
 // ADD EVENT LISTENERS
 button.addEventListener('click', ()=> {
-    firstLocation.textContent = nonsenseLocation.value;
-    pumpkin.textContent = veggiesInput.value;
-    woods.textContent = woodsInput.value;
+    
 
-    
-    
+    for (let x of vegetables){
+        x.textContent = veggiesInput.value;
+    }
+
+    for (let z of woods){
+        z.textContent = woodsInput.value;
+    }
+
+    for (let z of firstName){
+        z.textContent = firstNameInput.value;
+    }
+
+    for (let z of middleName){
+        z.textContent = middleNameInput.value;
+    }
+    for (let z of lastName){
+        z.textContent = lastNameInput.value;
+    }
+
+
+    replaceWords(firstLocation, nonsenseLocInput);
+    replaceWords(chairs, furnitureInput);
+    replaceWords(candle, fireInput);
+    replaceWords(drinkingVessel, jugInput);
 });
 
 
+
+
+//FUNCTION REPLACE WORDS
+    function replaceWords(span, input){
+    span.textContent = input.value;
+}
 
